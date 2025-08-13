4659fe8a2c03d74c6ed7ab8a5ffb9ebf4a76be06
ashasoman <ashasoman13@gmail.com>
2025-08-14 01:10:39 +1000

    ip changed

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.54.60.86:5001', // live
+  baseURL: 'http://3.25.106.233:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
